bb4e88be2708910db8e2494b039f89e9bd5af9ef
Meredith <mhu2@stuy.edu>
2018-02-05 11:46:09 -0500

    finished writing fib

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -10,14 +10,26 @@ public class Recursion{
 
     //a function that calculates a number in the fibonacci sequence whose index in the sequence is n, in linear time
     public int fib(int n){
-	
+	int fib1 = 0;
+	int fib2 = 1;
 	int sum = 0;
-	while(n>0){
+	while(n>1){
 	    if(n==0){
-		return 0;
+		sum=0;
+		n--;
 	    }
 	    if(n==1){
-		return 
+		sum=1;
+		n--;
+	    }
+	    sum = fib1+fib2;
+	    int temp = fib1;
+	    fib1 = fib2;
+	    fib2 = temp + fib1;
+	    n--;
+	}
+	return sum;
+    }
     
     /*
     public double sqrt(double n){
@@ -27,5 +39,12 @@ public class Recursion{
     public static void main(String[] args){
 	Recursion a = new Recursion();
 	System.out.println(a.fact(5));
+	System.out.println(a.fib(0));
+	System.out.println(a.fib(1));
+	System.out.println(a.fib(2));
+	System.out.println(a.fib(3));
+	System.out.println(a.fib(4));
+	System.out.println(a.fib(5));
+	System.out.println(a.fib(10));
     }
 }
