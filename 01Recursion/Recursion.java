7475d97fcbb6db313be9ac78aabd2e462c369b97
Meredith Hu <humeiyin@gmail.com>
2018-02-05 22:54:52 -0500

    the factorial function can't handle more than like, 13 since its return type is int, but that's what you wanted

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -31,14 +31,42 @@ public class Recursion{
 	return sum;
     }
     
-    /*
+    //a tail-recursive square-rooting function that calculates to 0.001 accuracy
     public double sqrt(double n){
+	if(n==0){
+	    System.out.println("0 doesn't have a square root you imbecile! You nearly broke the damn function!");
+	    return 0;
+	}
+	double guess = 1;
+	guess = (n/guess + guess)/2;
+	return help(n, guess);
     }
-    */
+
+    //the helper function for the square rooting function
+    public double help(double n, double guess){
+	guess = (n/guess + guess)/2;
+	if(guess/Math.sqrt(n)-1 < 0.001){
+	    return guess;
+	}
+	return help(n, guess);
+    }
 
     public static void main(String[] args){
+	//making new instance of Recusion object
 	Recursion a = new Recursion();
+
+	//testing factorial
+	System.out.println("factorial tests: ");
 	System.out.println(a.fact(5));
+	System.out.println(a.fact(0));
+	System.out.println(a.fact(1));
+	System.out.println(a.fact(2));
+	System.out.println(a.fact(10));
+	System.out.println(a.fact(20));//lmao the int overflowed
+	System.out.println();
+
+	//testing fibonacci
+	System.out.println("fibonacci tests: ");
 	System.out.println(a.fib(0));
 	System.out.println(a.fib(1));
 	System.out.println(a.fib(2));
@@ -46,5 +74,16 @@ public class Recursion{
 	System.out.println(a.fib(4));
 	System.out.println(a.fib(5));
 	System.out.println(a.fib(10));
+	System.out.println();
+
+	//testing square root
+	System.out.println("square root tests: ");
+	System.out.println(a.sqrt(100.0));
+	System.out.println(a.sqrt(4.0));
+	System.out.println(a.sqrt(1.0));
+	System.out.println(a.sqrt(0.0));
+	System.out.println(a.sqrt(2.0));
+	System.out.println(a.sqrt(3.0));
+	System.out.println(a.sqrt(5000000));
     }
 }
