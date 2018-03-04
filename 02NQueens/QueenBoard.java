04cc2bf5b57f38e9754a8f1c5e033bf33cbf913d
Meredith <mhu2@stuy.edu>
2018-03-04 10:49:05 -0500

    finished everything but countSolutions, which still does not work despite my best efforts

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -17,17 +17,17 @@ public class QueenBoard{
 	    ans+="\n";
 	    for(int g=0;g<board[0].length;g++){
 		if(board[i][g]==-1){
-		    ans+="Q";
-		    /*
+		    ans+="Q ";
 		}else{
-		    ans+="_";
+		    ans+="_ ";
 		}
-		    */
+		    /*
 		}else if(board[i][g]==0){
 		    ans+="0";
 		}else{
 		    ans+=board[i][g]+"";
 		}
+		    */
 	    }
 	}
 	return ans;
@@ -35,8 +35,8 @@ public class QueenBoard{
     
     private boolean addQueen(int r, int c){
 	if(board[r][c]==0){
-	    threaten(r,c);
 	    board[r][c]=-1;
+	    threaten(r,c);
 	    return true;
 	}else{
 	    return false;
@@ -56,20 +56,76 @@ public class QueenBoard{
 	for(int col=c+1;col<board.length;col++){
 	    board[r][col]++;
 	}
-	int looper=Math.max(r,c);
-	
-	
+	int row=r-1;
+	int col=c-1;
+	while(row>-1 && col>-1){
+	    board[row][col]++;
+	    row--;
+	    col--;
+	}
+	int rsw=r+1;
+	int csw=c-1;
+	while(rsw<board.length && csw>-1){
+	    board[rsw][csw]++;
+	    rsw++;
+	    csw--;
+	}
+	int rse=r+1;
+	int cse=c+1;
+	while(rse<board.length && cse<board.length){
+	    board[rse][cse]++;
+	    rse++;
+	    cse++;
+	}
+	int rne=r-1;
+	int cne=c+1;
+	while(rne>-1 && cne<board.length){
+	    board[rne][cne]++;
+	    rne--;
+	    cne++;
+	}
     }
 
     private void unthreaten(int r, int c){
-	int cols = board.length-c;
-	for(int cur =c+1;cur<cols;cur++){
-	    board[r][cur]--;
+	for(int row=r-1;row>-1;row--){
+	    board[row][c]--;
 	}
-	int col = c+1;
-	for(int row = r+1;row<cols;row++){
-		board[row][col]--;
-		col++;
+	for(int row=r+1;row<board.length;row++){
+	    board[row][c]--;
+	}
+	for(int col=c-1;col>-1;col--){
+	    board[r][col]--;
+	}
+	for(int col=c+1;col<board.length;col++){
+	    board[r][col]--;
+	}
+	int row=r-1;
+	int col=c-1;
+	while(row>-1 && col>-1){
+	    board[row][col]--;
+	    row--;
+	    col--;
+	}
+	int rsw=r+1;
+	int csw=c-1;
+	while(rsw<board.length && csw>-1){
+	    board[rsw][csw]--;
+	    rsw++;
+	    csw--;
+	}
+	int rse=r+1;
+	int cse=c+1;
+	while(rse<board.length && cse<board.length){
+	    board[rse][cse]--;
+	    rse++;
+	    cse++;
+	}
+	int rne=r-1;
+	int cne=c+1;
+	while(rne>-1 && cne<board.length){
+	    board[rne][cne]--;
+	    rne--;
+	    cne++;
 	}
     }
     
@@ -83,6 +139,16 @@ public class QueenBoard{
 	}
     }
 
+    public boolean solve(){
+	for(int i=0;i<board.length;i++){
+	    for(int g=0;g<board.length;g++){
+		if(board[i][g]!=0){
+		    throw new IllegalStateException("The board cannot start with any nonzero numbers");
+		}
+	    }
+	}
+	return this.solve(0);
+    }
     public boolean solve(int R){
 	if(R==board.length){
 	    return true;
@@ -98,19 +164,47 @@ public class QueenBoard{
 	return false;
     }
 
-    /*
     public int countSolutions(){
+	for(int i=0;i<board.length;i++){
+	    for(int g=0;g<board.length;g++){
+		if(board[i][g]!=0){
+		    throw new IllegalStateException("The board cannot start with any nonzero numbers");
+		}
+	    }
+	}
+	if(countSolutions(0)==-1){
+	    System.out.println("0 solutions exist for this board size");
+	    return -1;
+	}else{
+	    return countSolutions(0);
+	}
     }
-    */
+    public int countSolutions(int R){
+	int sols=0;
+	if(R==board.length){
+	    sols++;
+	    return sols;
+	}
+	for(int i=0;i<board.length;i++){
+	    if(addQueen(i,R)){
+		countSolutions(R+1);
+		removeQueen(i,R);
+	    }
+	}
+	return sols;
+    }
 
     public static void main(String[] args){
 	QueenBoard a = new QueenBoard(5);
 	System.out.println(a);
-	a.addQueen(3,2);
-	System.out.println(a);
+	//a.addQueen(3,0);
+	//a.addQueen(0,1);
+	//a.addQueen(2,2);
+	//System.out.println(a);
 	//a.removeQueen(2,2);
 	//System.out.println(a);
-	//System.out.println(a.solve(0));
+	//System.out.println(a.solve());
 	//System.out.println(a);
+	System.out.println(a.countSolutions());
     }
 }
