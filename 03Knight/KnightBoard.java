6bc363ad443affbd0414ccfaea503cd131da8da1
Meredith <mhu2@stuy.edu>
2018-03-04 14:48:18 -0500

    still doesn't workgit add . but it compiles now

--- 03Knight/KnightBoard.java
+++ 03Knight/KnightBoard.java
@@ -16,11 +16,69 @@ public class KnightBoard{
 
     public String toString(){
 	String ans="";
-	for(int i=0;i<startingRows;i++){
+	for(int i=0;i<board.length;i++){
 	    ans+="\n";
-	    for(int g=0;g<startingCols;g++){
-		ans+=board[i][g]+" ";
+	    for(int g=0;g<board[0].length;g++){
+		if(board[i][g]<10){
+		    ans+=" "+board[i][g]+" ";
+		}else{
+		    ans+=board[i][g]+" ";
+		}
 	    }
+	}
+	return ans;
+    }
+
+    public boolean solve(int startingRow, int startingCol){
+	if(startingRow<0 || startingCol<0){
+	    throw new IllegalArgumentException("Function parameters cannot be negative");
+	}
+	if(startingRow>board.length-1 || startingCol>board[0].length-1){
+	    throw new IllegalArgumentException("parameters specified are out of bounds of this particular board");
+	}
+	for(int i=0;i<board.length;i++){
+	    for(int g=0;g<board[0].length;g++){
+		if(board[i][g]!=0){
+		    throw new IllegalStateException("board cannot start with non-zero values");
+		}
+	    }
+	}
+
+	return solveH(startingRow,startingCol,1);
+    }
+    public boolean solveH(int row, int col, int level){
+	if(level==board.length*board[0].length){
+	    return true;
+	}
+	if(move(row,col,level)){
+	    if(solveH(row-2,col+1,level+1)||
+	       solveH(row-2,col-1,level+1)||
+	       solveH(row-1,col+2,level+1)||
+	       solveH(row-1,col-2,level+1)||
+	       solveH(row+2,col+1,level+1)||
+	       solveH(row+2,col-1,level+1)||
+	       solveH(row+1,col+2,level+1)||
+	       solveH(row+1,col-2,level+1)){
+		return true;
+	    }else{
+		board[row][col]=0;
+	    }
+	}
+	return false;
+    }
+
+    public boolean move(int row, int col, int level){
+	if(row<0 || col<0){
+	    return false;
+	}
+	if(row>board.length-1 || col>board[0].length-1){
+	    return false;
+	}
+	if(board[row][col]!=0){
+	    return false;
+	}else{
+	    board[row][col]=level;
+	    return true;
 	}
     }
 }
--- 03Knight/KnightDriver.java
+++ 03Knight/KnightDriver.java
@@ -1,6 +1,9 @@
 public class KnightDriver{
     public static void main(String[] args){
-	KnightBoard a = new KnightBoard(5);
+	KnightBoard a = new KnightBoard(5,5);
+	System.out.println(a);
+	//a.solve(2,2);
+	System.out.println(a.solve(2,2));
 	System.out.println(a);
     }
 }
