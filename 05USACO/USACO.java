c987fa497563259374f134769f09ddb7077347ce
Meredith Hu <humeiyin@gmail.com>
2018-03-05 22:56:58 -0500

    started

---
+++ 05USACO/USACO.java
@@ -0,0 +1,14 @@
+public class USACO{
+
+    public static int bronze(String filename){
+	return 0;
+    }
+
+    public static int silver(String filename){
+	return 0;
+    }
+
+    public static void main(String[] args){
+    
+    }
+}
