4a7a27dca0e22232b909408d1265af6c353edaf0
Meredith Hu <humeiyin@gmail.com>
2018-03-17 00:03:02 -0400

    fixed nothing

--- 06Quick/QuickDriver.java
+++ 06Quick/QuickDriver.java
@@ -3,6 +3,7 @@ public class QuickDriver{
 	int[] a = {3, 2, 7, 8, 9, 4, 5, 0, 6, 1};
 	Quick.swap(a,0,1);
 	System.out.println(a[0]);
-	System.out.println(Quick.partition(a,0,a.length-1));
+	System.out.println(a[1]);
+	//System.out.println(Quick.partition(a,0,a.length-1));
     }
 }
