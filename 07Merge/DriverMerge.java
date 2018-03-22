e0ea169a3594a7c4b1eee3ca7f4de0393188461a
Meredith Hu <humeiyin@gmail.com>
2018-03-22 00:36:15 -0400

    I'm very tired and sad because it only works halfway and I've been trying to code it for half a day. I guess I'll just keep working on it tomorrow, or I'll give up and code it with making a new array copy every time it splits in half if I don't figure it out by tomorrow

--- 07Merge/DriverMerge.java
+++ 07Merge/DriverMerge.java
@@ -2,8 +2,15 @@
 import java.util.*;
 public class DriverMerge{
     public static void main(String[] args){
-	int[] a = {1,3,5,7,2,4,6,8};
+	int[] a = {4,1,5,8};
+	int[] b = {4,3,2,1};
+	int[] c = {1,3,5,7,2,4,6,8};
 	//System.out.println(Arrays.toString(a));
 	Merge.mergesort(a);
+	System.out.println(Arrays.toString(a));
+	Merge.mergesort(b);
+	System.out.println(Arrays.toString(b));
+	Merge.mergesort(c);
+	System.out.println(Arrays.toString(c));
     }
 }
--- 07Merge/Merge.java
+++ 07Merge/Merge.java
@@ -4,24 +4,61 @@ public class Merge{
 	if(data.length<1){
 	    throw new IllegalArgumentException();
 	}
-	int[] temp = new int[data.length-1];
+	int[] temp = new int[data.length];
 	msort(data,temp,0,data.length-1);
     }
     private static void msort(int[] data, int[] temp, int lo, int hi){
 	if(lo>=hi){
 	    return;
-	}
+	}	
 	//System.out.println(Arrays.toString(temp));
-	for(int n:data){
-	    temp[n]=data[n];
+	for(int i=lo;i<hi;i++){
+	    temp[i]=data[i];
 	}
 	//System.out.println(Arrays.toString(data));
 	//System.out.println(Arrays.toString(temp));
 	int mid = (hi+lo)/2;
-	//msort(temp, data, lo, mid);
-	//msort(temp, data, mid+1, hi);
+	msort(temp, data, lo, mid);
+	msort(temp, data, mid+1, hi);
+	merge(data, temp, lo, mid, hi);
     }
-    private static void merge(int[] data, int[] temp, int hi, int mid, int lo){
-	return;
+    private static void merge(int[] data, int[] temp, int lo, int mid, int hi){
+	int i1=lo;
+	int i2=mid+1;
+	int index = lo;
+	/*
+	if(i1==i2){
+	    return;
+	}
+	if(i2-i1==1){
+	    if(temp[i1]<=temp[i1+1]){
+		data[index]=temp[i1];
+		data[index+1]=temp[i1+1];
+		System.out.println(Arrays.toString(data));
+		return;
+	    }else{
+		data[index]=temp[i1+1];
+		data[index+1]=temp[i1];
+		System.out.println(Arrays.toString(data));
+		return;
+	    }
+	}
+	*/
+	while(i1<=mid || i2<=hi){
+	    if(i1>mid){
+		data[index]=temp[i2];
+		i2++;
+	    }else if(i2>hi){
+		data[index]=temp[i1];
+		i1++;
+	    }else if(temp[i1]<=temp[i2]){
+		data[index]=temp[i1];
+		i1++;
+	    }else{
+		data[index]=temp[i2];
+		i2++;
+	    }
+	    index++;
+	}
     }
 }
