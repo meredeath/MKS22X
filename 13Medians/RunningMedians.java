afca31ffa36edb7c95946f4f72e546ac18f86095
Meredith <mhu2@stuy.edu>
2018-04-30 15:32:44 -0400

    started RunningMedians

---
+++ 13Medians/RunningMedians.java
@@ -0,0 +1,13 @@
+public class RunningMedians{
+    int minSize;
+    int maxSize;
+    
+    public static double getMedian(){
+	return 0;
+    }
+    public static void add(double i){
+	return;
+    }
+    public static void rebalance(){
+    }
+}
