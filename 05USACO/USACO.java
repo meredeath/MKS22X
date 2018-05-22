c52d4bbf070183a6d500d3354eeaa41289aff8e7
Meredith <mhu2@stuy.edu>
2018-05-22 16:59:37 -0400

    doesn't compile

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -2,12 +2,32 @@ import java.util.*;
 import java.io.*;
 public class USACO{
 
-    public static int bronze(String filename){
+    public static int bronze(String filename) throws FileNotFoundException{
 	File f = new File(filename);
-	if(!f.exists()){
-	    throw new FileNotFoundException("hey!! your file does not exist!");
+	Scanner scan = new Scanner(f);
+	ArrayList<String> stuff = new ArrayList<>();
+	while(scan.hasNextLine()){
+	    String s = scan.nextLine();
+	    stuff.add(s);
 	}
-	
+	//System.out.println(stuff);
+
+	int r,c,e,n;
+	String r1=stuff.get(0);
+	String[] ins = r1.split(" ");
+	r=Integer.parseInt(ins[0]);
+	c=Integer.parseInt(ins[1]);
+	e=Integer.parseInt(ins[2]);
+	n=Integer.parseInt(ins[3]);
+	int[][] ground= new int[r][c];
+	for(int i=1;i<c+1;i++){
+	    String s = stuff.get(i);
+	    String[] row = s.split(" ");
+	    for(int g=0;g<s.length();i++){
+		ground[i][0]=Integer.parseInt(row[g]);
+	    }
+	}
+	System.out.println(Arrays.toString(ins));
 	return 0;
     }
 
@@ -16,6 +36,11 @@ public class USACO{
     }
 
     public static void main(String[] args){
-    
+	try{
+	    USACO.bronze("test1.txt");
+	}catch(FileNotFoundException e){
+	    System.out.println("your file does not exist!");
+	    System.exit(1);
+	}
     }
 }
