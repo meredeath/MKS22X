3539880477816f9b3cec809c9bc1af9f25286726
Meredith Hu <humeiyin@gmail.com>
2018-05-28 16:48:42 -0400

    partition works (not sure why, shoutout to Imad although he doesn't know why either), quickselect doesn't. why is progress so slow? although progress is progressively getting faster.

--- 06Quick/Quick.java
+++ 06Quick/Quick.java
@@ -1,9 +1,48 @@
-import java.util.Random;
+import java.util.*;
 public class Quick{
     public static int partition(int[] data, int start, int end){
-	Random ran = new Random();
-	int pi = ran.nextInt(data.length);
+	//select a random pivot
+	int pi = (int)(Math.random() * (end - start + 1) + start);
+	int v = data[pi];
+
+	//move the pivot to the front
+	swap(data,start,pi);
+
+	//swap sides around pivot
+	//when small and large pass each other, stop
+	int i=start+1;
+	while(i<=end){
+	    if(data[i]<v){
+		swap(data,i,start);
+		start++;
+		i++;
+	    }else if(data[i]>v){
+		swap(data,i,end);
+		end--;
+	    }else{
+		i++;
+	    }
+	}
 	return pi;
+    }
+
+    public static int quickselect(int[] data, int k){
+	//return the value that is the kth smallest value of the array. 
+	//use your partition method to help you accomplish this.
+	return quickselect(data,0,data.length-1,k);
+    }
+    public static int quickselect(int[] data, int start, int end, int k){
+	int piv = partition(data,start,end);
+	if(piv==k){
+	    return data[piv];
+	}else if(piv<k){
+	    return quickselect(data,start,piv,k);
+	}else{
+	    return quickselect(data,piv,end,k);
+	}
+    }
+
+    public static void quicksort(int[] ary){
     }
 
     public static void swap(int[] data, int i, int j){
--- 06Quick/QuickDriver.java
+++ 06Quick/QuickDriver.java
@@ -1,9 +1,13 @@
+import java.util.*;
 public class QuickDriver{
     public static void main(String[] args){
 	int[] a = {3, 2, 7, 8, 9, 4, 5, 0, 6, 1};
-	Quick.swap(a,0,1);
-	System.out.println(a[0]);
-	System.out.println(a[1]);
-	//System.out.println(Quick.partition(a,0,a.length-1));
+	//System.out.println(Arrays.toString(a));
+	//Quick.swap(a,0,1);
+	System.out.println(Arrays.toString(a));
+	Quick.partition(a,0,a.length-1);
+	System.out.println(Arrays.toString(a));
+
+	//System.out.println(Quick.quickselect(a,4));
     }
 }
