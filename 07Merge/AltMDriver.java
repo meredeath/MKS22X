8de75431ba4deaf21f6a1adae394f76222d74874
Meredith Hu <humeiyin@gmail.com>
2018-06-01 01:17:35 -0400

    pushing changes?

--- 07Merge/AltMDriver.java
+++ 07Merge/AltMDriver.java
@@ -68,6 +68,8 @@ public class AltMDriver{
 	/*
 	 * Test your sort here!
 	 */
+	Merge.mergesort(start);
+
 	long elapsedTime = System.currentTimeMillis() - startTime;
 	if(Arrays.equals(start,result)){
 	    System.out.println("PASS Case "+name(type)+" array, size:"+size+" "+elapsedTime/1000.0+"sec ");
