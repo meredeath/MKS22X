004357d6939c8b3001b1ce1276022d0396be6f79
Meredith Hu <humeiyin@gmail.com>
2018-06-03 21:48:43 -0400

    wrote a skeleton and started on a method. will definitely finish tomorrow.

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -1,6 +1,72 @@
-public class MyDeque{
-    int size;
+import java.util.*;
+public class MyDeque<E>{
+    private int size;
+    private int front,back;
+    private E[] arr;
+
+    @SuppressWarnings("unchecked")
     public MyDeque(){
 	size=0;
+	front=0;
+	back=9;
+	arr= (E[])new Object[10];
+    }
+
+    @SuppressWarnings("unchecked")
+    public MyDeque(int initialCapacity){
+	if(initialCapacity<0){
+	    throw new IllegalArgumentException("hey! you can't have a negative sized deque!");
+	}
+	size=0;
+	front=0;
+	back=initialCapacity-1;
+	arr=(E[])new Object[initialCapacity];
+    }
+
+    public int size(){
+	return size;
+    }
+
+    /*
+    public void resize(){
+	arr = (E[])new Object[size*2];
+    }
+    */
+
+    public void addFirst(E e){
+	if(e==null){
+	    throw new NullPointerException("you can't add null...");
+	}
+    }
+    public void addLast(E e){
+	if(e==null){
+	    throw new NullPointerException("you can't add null...");
+	}
+    }
+    
+    public E removeFirst(){
+	if(size==0){
+	    throw new NoSuchElementException("there is nothing to remove");
+	}
+	return arr[0];
+    }
+    public E removeLast(){
+	if(size==0){
+	    throw new NoSuchElementException("there is nothing to remove");
+	}
+	return arr[0];
+    }
+
+    public E getFirst(){
+	if(size==0){
+	    throw new NoSuchElementException("there is nothing to remove");
+	}
+	return arr[0];
+    }
+    public E getLast(){
+	if(size==0){
+	    throw new NoSuchElementException("there is nothing to remove");
+	}
+	return arr[0];
     }
 }
