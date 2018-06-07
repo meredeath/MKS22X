758b4cfa3e19e57c5225c3681d2db0c4dc8ef2a0
Meredith Hu <humeiyin@gmail.com>
2018-06-07 00:11:08 -0400

    oh no, only best-first and a-star work. depth first and breadth first don't, and at this point in the night I'm too tired and scared to trace and see why

--- 14Frontier/MazeSolver.java
+++ 14Frontier/MazeSolver.java
@@ -1,8 +1,13 @@
 public class MazeSolver{
     private Maze maze;
     private Frontier frontier;
+    private boolean animate;
+    private boolean astar;
     
     public MazeSolver(String mazeText){
+	animate=false;
+	astar=false;
+	maze = new Maze(mazeText);
     }
     
     //Default to BFS
@@ -11,6 +16,8 @@ public class MazeSolver{
     //mode: required to allow for alternate solve modes.
     //0: BFS
     //1: DFS
+    //2: Best-First Search
+    //3: A-Star Search
     public boolean solve(int mode){
 	//initialize your frontier
 	//while there is stuff in the frontier:
@@ -19,7 +26,47 @@ public class MazeSolver{
 	//  check if any locations are the end, if you found the end just return true!
 	//  add all the locations to the frontier
 	//when there are no more values in the frontier return false
+	if(mode==0){
+	    frontier = new QueueFrontier();
+	}else if(mode==1){
+	    frontier=new StackFrontier();
+	}else if (mode==2){
+	    frontier = new PriorityQueueFrontier();
+	}else{
+	    frontier = new PriorityQueueFrontier();
+	    astar=true;
+	}
+	boolean done = false;
+	frontier.add(maze.getStart());
+	while(!done && !frontier.hasNext()){
+	    Location cur = frontier.next();
+	    maze.set(cur.getX(),cur.getY(),'.');
+	    Location[] neighbors = maze.getNeighbors(cur,astar);
+	    for(int i=0;i<neighbors.length;i++){
+		if(neighbors[i]!=null){
+		    if(maze.get(neighbors[i].getX(),neighbors[i].getY())=='E'){
+			Location path = neighbors[i];
+			while(path.hasPrev()){
+			    path = path.getPrev();
+			    maze.set(path.getX(),path.getY(),'@');
+			}
+			done=true;
+			return true;
+		    }else{
+			frontier.add(neighbors[i]);
+			maze.set(neighbors[i].getX(),neighbors[i].getY(),'*');
+		    }
+		}
+	    }
+	    if(animate){
+		System.out.println(maze.toStringColor());
+	    }
+	}
 	return false;
+    }
+
+    public void setAStar(boolean b){
+	astar=b;
     }
 
     public String toString(){
